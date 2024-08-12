6623653842828839dc3c3a63040a6fc06a120798
hanguojun007 <2867965897@qq.com>
2024-08-12 21:57:43 +0800

    20240812

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # shinyheatmmapforHDY
+
 For Brother
+
+老何，直接跑下面的代码。缺失包就安装一下
+
+```
+# install.packages("shiny")
+shiny::runGitHub("shinyheatmmapforHDY", "hanguojun007")
+```
---
+++ app.R
@@ -0,0 +1,61 @@
+library(shiny)
+library(reactable)
+
+ui <- fluidPage(
+  titlePanel("Heatmap"),
+  fluidRow(
+    column(
+      4,
+      fileInput("file", label = NULL, placeholder = "Upload File", accept = c("tsv", "csv")),
+      checkboxGroupInput("cols", "Columns", choices = NULL, selected = NULL)
+    ),
+    column(8, reactable::reactableOutput("table"))
+  ),
+  fluidRow(
+    column(
+      4,
+      checkboxInput("show_rownames", "Show Row Names", value = TRUE),
+      checkboxInput("show_colnames", "Show Column Names", value = TRUE)
+    ),
+    column(8, plotOutput("heatmap"))
+  )
+)
+
+server <- function(input, output, session) {
+  data <- reactive({
+    req(input$file)
+    vroom::vroom(input$file$datapath)
+  })
+
+  output$table <- renderReactable({
+    reactable(data(), paginationType = "jump", selection = "multiple", onClick = "select", defaultSelected = c(1:nrow(data())))
+  })
+  # req(data())
+  selected <- reactive(getReactableState("table", "selected"))
+
+  observeEvent(data(), {
+    updateCheckboxGroupInput(session, "cols", choices = colnames(data()), selected = colnames(data()))
+  })
+
+  col_selected <- reactive({
+    req(input$cols)
+    input$cols
+  })
+
+  showRownames <- reactive(input$show_rownames)
+  showColnames <- reactive(input$show_colnames)
+
+  output$heatmap <- renderPlot(
+    {
+      ComplexHeatmap::Heatmap(
+        as.matrix(data()[selected(), col_selected()]),
+        show_row_names = showRownames(),
+        show_column_names = showColnames()
+      )
+    },
+    res = 96
+  )
+}
+
+
+shinyApp(ui, server)
